b43dd3be6f8f8a14e2d052824bafec6cfb685ca2
seeprabebe <sinisimmuseepra@gmail.com>
2025-03-29 17:01:01 +0200

    app.py

--- app.py
+++ app.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/")
+def index():
+    db = sqlite3.connect("database.db")
+    messages = db.execute("SELECT content FROM messages").fetchall()
+    db.close()
+    count = len(messages)
+    return render_template("index.html", count=count, messages=messages)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    db = sqlite3.connect("database.db")
+    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
+    db.commit()
+    db.close()
+    return redirect("/")
+
 if __name__ == "__main__":
     app.run(debug=True)
