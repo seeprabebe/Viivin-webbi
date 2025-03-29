0439ee7cf7f4cfa6fc95b6b4c3f03503710dcdcb
seeprabebe <sinisimmuseepra@gmail.com>
2025-03-29 17:04:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,231 @@
-from flask import Flask, render_template
+import sqlite3, math, secrets
+from flask import Flask
+from flask import abort, flash, make_response, redirect, render_template, request, session
+import config, forum, users
+import markupsafe
+from flask import g
+import time
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
+
+@app.before_request
+def before_request():
+    g.start_time = time.time()
+
+@app.after_request
+def after_request(response):
+    elapsed_time = round(time.time() - g.start_time, 2)
+    print("elapsed time:", elapsed_time, "s")
+    return response
+
+@app.template_filter()
+def show_lines(content):
+    content = str(markupsafe.escape(content))
+    content = content.replace("\n", "<br />")
+    return markupsafe.Markup(content)
+
+def require_login():
+    if "user_id" not in session:
+        abort(403)
+
+def check_csrf():
+    if request.form["csrf_token"] != session["csrf_token"]:
+        abort(403)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/<int:page>")
+def index(page=1):
+    page_size = 10
+    thread_count = forum.thread_count()
+    page_count = math.ceil(thread_count / page_size)
+    page_count = max(page_count, 1)
 
-@app.route("/")
-def index():
-    db = sqlite3.connect("database.db")
-    messages = db.execute("SELECT content FROM messages").fetchall()
-    db.close()
-    count = len(messages)
-    return render_template("index.html", count=count, messages=messages)
+    if page < 1:
+        return redirect("/1")
+    if page > page_count:
+        return redirect("/" + str(page_count))
 
-@app.route("/new")
-def new():
-    return render_template("new.html")
+    threads = forum.get_threads(page, page_size)
+    return render_template("index.html", page=page, page_count=page_count, threads=threads)
 
-@app.route("/send", methods=["POST"])
-def send():
+
+@app.route("/search")
+def search():
+    query = request.args.get("query")
+    results = forum.search(query) if query else []
+    return render_template("search.html", query=query, results=results)
+
+@app.route("/user/<int:user_id>")
+def show_user(user_id):
+    user = users.get_user(user_id)
+    if not user:
+        abort(404)
+    messages = users.get_messages(user_id)
+    return render_template("user.html", user=user, messages=messages)
+
+@app.route("/thread/<int:thread_id>")
+def show_thread(thread_id):
+    thread = forum.get_thread(thread_id)
+    if not thread:
+        abort(404)
+    messages = forum.get_messages(thread_id)
+    return render_template("thread.html", thread=thread, messages=messages)
+
+@app.route("/new_thread", methods=["POST"])
+def new_thread():
+    require_login()
+
+    title = request.form["title"]
     content = request.form["content"]
-    db = sqlite3.connect("database.db")
-    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
-    db.commit()
-    db.close()
+    if not title or len(title) > 100 or len(content) > 5000:
+        abort(403)
+    user_id = session["user_id"]
+
+    thread_id = forum.add_thread(title, content, user_id)
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/new_message", methods=["POST"])
+def new_message():
+    check_csrf()
+    require_login()
+
+    content = request.form["content"]
+    if len(content) > 5000:
+        abort(403)
+    user_id = session["user_id"]
+    thread_id = request.form["thread_id"]
+
+    try:
+        forum.add_message(content, user_id, thread_id)
+    except sqlite3.IntegrityError:
+        abort(403)
+
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/edit/<int:message_id>", methods=["GET", "POST"])
+def edit_message(message_id):
+    require_login()
+
+    message = forum.get_message(message_id)
+    if not message or message["user_id"] != session["user_id"]:
+        abort(403)
+
+    if request.method == "GET":
+        return render_template("edit.html", message=message)
+
+    if request.method == "POST":
+        content = request.form["content"]
+        if len(content) > 5000:
+            abort(403)
+        forum.update_message(message["id"], content)
+        return redirect("/thread/" + str(message["thread_id"]))
+
+@app.route("/remove/<int:message_id>", methods=["GET", "POST"])
+def remove_message(message_id):
+    require_login()
+
+    message = forum.get_message(message_id)
+    if not message or message["user_id"] != session["user_id"]:
+        abort(403)
+
+    if request.method == "GET":
+        return render_template("remove.html", message=message)
+
+    if request.method == "POST":
+        if "continue" in request.form:
+            forum.remove_message(message["id"])
+        return redirect("/thread/" + str(message["thread_id"]))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html", filled={})
+
+    if request.method == "POST":
+        username = request.form["username"]
+        if not username or len(username) > 16:
+            abort(403)
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+
+        if password1 != password2:
+            flash("VIRHE: Antamasi salasanat eivät ole samat")
+            filled = {"username": username}
+            return render_template("register.html", filled=filled)
+
+        try:
+            users.create_user(username, password1)
+            flash("Tunnuksen luominen onnistui, voit nyt kirjautua sisään")
+            return redirect("/")
+        except sqlite3.IntegrityError:
+            flash("VIRHE: Valitsemasi tunnus on jo varattu")
+            filled = {"username": username}
+            return render_template("register.html", filled=filled)
+        
+    return "Tunnus luotu"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", next_page=request.referrer)
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        next_page = request.form["next_page"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            session["csrf_token"] = secrets.token_hex(16)
+            return redirect(next_page)
+        else:
+            flash("VIRHE: Väärä tunnus tai salasana")
+            return render_template("login.html", next_page=next_page)
+        
+@app.route("/logout")
+def logout():
+    require_login()
+
+    del session["user_id"]
     return redirect("/")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/add_image", methods=["GET", "POST"])
+def add_image():
+    require_login()
+
+    if request.method == "GET":
+        return render_template("add_image.html")
+    
+    if request.method == "POST":
+        check_csrf()
+
+        file = request.files["image"]
+        if not file.filename.endswith(".jpg"):
+            flash("VIRHE: Lähettämäsi tiedosto ei ole jpg-tiedosto")
+            return redirect("/add_image")
+
+        image = file.read()
+        if len(image) > 100 * 1024:
+            flash("VIRHE: Lähettämäsi tiedosto on liian suuri")
+            return redirect("/add_image")
+
+        user_id = session["user_id"]
+        users.update_image(user_id, image)
+        flash("Kuvan lisääminen onnistui")
+        return redirect("/user/" + str(user_id))
+    
+@app.route("/image/<int:user_id>")
+def show_image(user_id):
+    image = users.get_image(user_id)
+    if not image:
+        abort(404)
+
+    response = make_response(bytes(image))
+    response.headers.set("Content-Type", "image/jpeg")
+    return response
+
+if __name__=='__main__':
+    app.run(host= '0.0.0.0', port=5000)
+    
---
+++ users.py
@@ -0,0 +1,46 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+import db
+
+def create_user(username, password):
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])
+
+    if len(result) == 1:
+        user_id, password_hash = result[0]
+        if check_password_hash(password_hash, password):
+            return user_id
+
+    return None
+
+def get_user(user_id):
+    sql = """SELECT id, username, image IS NOT NULL has_image
+             FROM users
+             WHERE id = ?"""
+    result = db.query(sql, [user_id])
+    return result[0] if result else None
+
+def get_messages(user_id):
+    sql = """SELECT m.id,
+                    m.thread_id,
+                    t.title thread_title,
+                    m.sent_at
+             FROM threads t, messages m
+             WHERE t.id = m.thread_id AND
+                   m.user_id = ?
+             ORDER BY m.sent_at DESC"""
+    return db.query(sql, [user_id])
+
+
+def update_image(user_id, image):
+    sql = "UPDATE users SET image = ? WHERE id = ?"
+    db.execute(sql, [image, user_id])
+
+def get_image(user_id):
+    sql = "SELECT image FROM users WHERE id = ?"
+    result = db.query(sql, [user_id])
+    return result[0][0] if result else None
