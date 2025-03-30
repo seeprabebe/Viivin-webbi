977d90fc139e727272fe12e350c0c936288014e5
seeprabebe <sinisimmuseepra@gmail.com>
2025-03-30 10:27:18 +0300

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    message = request.form["message"]
+    return render_template("result.html", message=message)
 
 if __name__ == "__main__":
     app.run(debug=True)
