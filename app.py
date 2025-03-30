eb12f3f021079d1649df9122096d47b6a808156b
seeprabebe <sinisimmuseepra@gmail.com>
2025-03-30 10:33:21 +0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -10,10 +11,27 @@ def index():
 def form():
     return render_template("form.html")
 
-@app.route("/result", methods=["POST"])
-def result():
-    message = request.form["message"]
-    return render_template("result.html", message=message)
+@app.route("/")
+def index():
+    db = sqlite3.connect("database.db")
+    messages = db.execute("SELECT content FROM messages").fetchall()
+    db.close()
+    count = len(messages)
+    return render_template("index.html", count=count, messages=messages)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    db = sqlite3.connect("database.db")
+    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
+    db.commit()
+    db.close()
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
